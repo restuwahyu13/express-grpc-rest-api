d2caa3c928b90cd9285b28d04d77c4d970a3c219
restuwahyu13 <restuwahyu13@gmail.com>
2021-01-22 21:03:17 +0700

    AddUpdateServiceClient

--- src/client/controllers/mahasiswa/mhs.update.ts
+++ src/client/controllers/mahasiswa/mhs.update.ts
@@ -1,5 +1,39 @@
 import { Request, Response } from 'express'
+import { ServiceError } from '@grpc/grpc-js'
+import { grpcClient } from '../../middlewares/middleware.grpc'
+import { StudentRequest, StudentResponse } from '../../../typedefs/mahasiswa_pb'
+import { grpcMessage } from '../../utils/util.message'
 
-export const updateStudent = (req: Request, res: Response): Response<any, Record<string, any>> => {
-	return res.status(200).send('hello wordl')
+export const updateStudent = (req: Request, res: Response): void => {
+	const client = grpcClient()
+	const payloadBody = new StudentRequest()
+	payloadBody.setId(req.params.id)
+	payloadBody.setName(req.body.name)
+	payloadBody.setNpm(req.body.npm)
+	payloadBody.setFak(req.body.fak)
+	payloadBody.setBid(req.body.bid)
+
+	client.updateStudent(params, (error: ServiceError, response: StudentResponse): void => {
+		if (error) {
+			grpcMessage(res, {
+				method: req.method,
+				statusCode: +response.getStatuscode(),
+				message: response.getMessage()
+			})
+		}
+
+		if (response !== undefined && response.toArray().length > 0) {
+			grpcMessage(res, {
+				method: req.method,
+				statusCode: +response.getStatuscode(),
+				message: response.getMessage()
+			})
+		} else {
+			grpcMessage(res, {
+				method: req.method,
+				statusCode: +response.getStatuscode(),
+				message: response.getMessage()
+			})
+		}
+	})
 }
