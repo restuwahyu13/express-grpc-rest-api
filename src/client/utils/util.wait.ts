b2dc13b57d851d9f29ac79bb824a0e492c01dbed
restuwahyu13 <restuwahyu13@gmail.com>
2021-02-02 23:56:12 +0700

    remove util.wait and uti.error

--- src/client/utils/util.wait.ts
+++
@@ -1,4 +0,0 @@
-import wait, { ClearablePromise } from 'delay'
-
-export { ClearablePromise }
-export const waitFor = (delay?: number): ClearablePromise<void> => (delay !== undefined || null ? wait(delay) : wait(3000))
